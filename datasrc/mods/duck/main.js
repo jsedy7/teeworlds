beb1c7eb08b3e524b95403a9abcc73eb18422a93
LordSk <lordskelethom@gmail.com>
2019-03-03 16:47:59 +0100

    Copy js files with bam

--- datasrc/mods/duck/main.js
+++ datasrc/mods/duck/main.js
@@ -2,8 +2,8 @@
 
 print('Ducktape version = ' + Duktape.version);
 
-function OnUpdate(someInt)
+function OnUpdate(clientLocalTime)
 {
     //print("Hello from the dark side! " + someInt);
-    RenderQuad(10.0, 10.0, 100.0, 50.0);
+    RenderQuad(10.0, 10.0, 100.0 + Math.sin(clientLocalTime) * 50.0, 50.0);
 }
