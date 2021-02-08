5f141407af697ce483e4ff93e95101142f9d3c56
Jiří Šedý <jsedy7@gmail.com>
2021-02-08 18:04:09 +0100

    Update character.cpp

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -432,7 +432,7 @@ void CCharacter::FireWeapon()
 	m_AttackTick = Server()->Tick();
 
 	if(m_aWeapons[m_ActiveWeapon].m_Ammo > 0) // -1 == unlimited
-		m_aWeapons[m_ActiveWeapon].m_Ammo--;
+		m_aWeapons[m_ActiveWeapon].m_Ammo;
 
 	if(!m_ReloadTimer)
 		m_ReloadTimer = g_pData->m_Weapons.m_aId[m_ActiveWeapon].m_Firedelay * Server()->TickSpeed() / 1000;
